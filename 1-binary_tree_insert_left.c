8c7d403e11ad75ef3e1d71d45eb650f275ab4d70
Henzz <khenok53@gmail.com>
2024-03-01 03:00:14 +0300

    task 2 update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,13 +18,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->left = parent->left;
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
 
 	parent->left = new_node;
+	new_node->parent = parent;
 
 	return (new_node);
 }
